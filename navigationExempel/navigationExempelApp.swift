4dea7a62ba8e68902a0c1dac01352bc3f9e8ee55
nz-bill <bill.palmestedt@zocom.se>
2024-01-31 09:12:29 +0100

    Initial Commit

---
+++ navigationExempel/navigationExempelApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationExempelApp.swift
+//  navigationExempel
+//
+//  Created by Bill Palmestedt on 2024-01-31.
+//
+
+import SwiftUI
+
+@main
+struct navigationExempelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
